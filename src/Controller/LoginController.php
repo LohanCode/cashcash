08f1c2a5a02632756cbf246a495179e94f2d3ae2
Lohan Poulain <lohanpou@yahoo.fr>
2025-12-15 18:15:23 +0100

    Probleme login régler

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,22 +11,20 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-public function index(AuthenticationUtils $authenticationUtils, Request $request): Response
-{
-    // Si l'utilisateur est déjà connecté, on le déconnecte
-    if ($this->getUser() && $request->query->get('action') === 'logout') {
-        $this->get('security.token_storage')->setToken(null);
-        $request->getSession()->invalidate();
-        return $this->redirectToRoute('app_login');
+    public function index(AuthenticationUtils $authenticationUtils, Request $request): Response
+    {
+        if ($this->getUser() && $request->query->get('action') === 'logout') {
+            $this->get('security.token_storage')->setToken(null);
+            $request->getSession()->invalidate();
+            return $this->redirectToRoute('app_login');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
-
-    // Gestion de la connexion normale
-    $error = $authenticationUtils->getLastAuthenticationError();
-    $lastUsername = $authenticationUtils->getLastUsername();
-
-    return $this->render('login.html.twig', [
-        'last_username' => $lastUsername,
-        'error' => $error,
-    ]);
-}
 }
