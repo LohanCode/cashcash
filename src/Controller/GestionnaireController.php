22ac8896fe011377fa0dfcb47e58dc625ed432dd
Lohan Poulain <lohanpou@yahoo.fr>
2025-12-15 16:57:06 +0100

    changement

--- src/Controller/GestionnaireController.php
+++ src/Controller/GestionnaireController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\InterventionRepository;
-use App\Repository\EmployeRepository;
+use App\Repository\UtilisateurRepository;
 use App\Repository\ClientRepository;
 
 class GestionnaireController extends AbstractController
@@ -25,60 +25,90 @@ class GestionnaireController extends AbstractController
     public function affectation(
         Request $request,
         InterventionRepository $interventionRepository,
-        EmployeRepository $employeRepository,
+        UtilisateurRepository $utilisateurRepository,
         ClientRepository $clientRepository,
         EntityManagerInterface $em
     ): Response {
-        if ($request->isMethod('POST')) {
-            $interventionId = $request->request->get('intervention_id');
-            $technicienId   = $request->request->get('technicien_id');
-            $clientId       = $request->request->get('client_id');
-            $dateVisiteStr  = $request->request->get('date_visite');
-            $heureVisiteStr = $request->request->get('heure_visite');
+if ($request->isMethod('POST')) {
+    $interventionId = $request->request->get('intervention_id');
+    $technicienId   = $request->request->get('technicien_id');
+    $clientId       = $request->request->get('client_id');
+    $dateVisiteStr  = $request->request->get('date_visite');
+    $heureVisiteStr = $request->request->get('heure_visite');
 
-            if ($interventionId && $technicienId) {
-                $intervention = $interventionRepository->find($interventionId);
-                $technicien   = $employeRepository->find($technicienId);
-                $client       = $clientId ? $clientRepository->find($clientId) : null;
-
-                if ($intervention && $technicien) {
-                    $intervention->setTechnicien($technicien);
-                    if ($client) {
-                        $intervention->setClient($client);
-                    }
-
-                    if (!empty($dateVisiteStr)) {
-                        try {
-                            $dateVisite = new \DateTime($dateVisiteStr);
-                            $intervention->setDateVisite($dateVisite);
-                        } catch (\Exception $e) {
-                            $this->addFlash('danger', 'Format de date invalide.');
-                        }
-                    }
-
-                    if (!empty($heureVisiteStr)) {
-                        try {
-                            $heureVisite = new \DateTime($heureVisiteStr);
-                            $intervention->setHeureVisite($heureVisite);
-                        } catch (\Exception $e) {
-                            $this->addFlash('danger', 'Format d\'heure invalide.');
-                        }
-                    }
-                    $em->flush();
-                    $this->addFlash('success', 'Intervention affectée au technicien.');
-
-                } else {
-                    $this->addFlash('danger', "Intervention ou technicien introuvable.");
-                }
-            } else {
-                $this->addFlash('danger', 'Données d\'affectation incomplètes.');
-            }
-
+    if ($interventionId && $technicienId) {
+        $intervention = $interventionRepository->find($interventionId);
+        // Vérification que le technicien existe ET est bien de type technicien
+        $technicien = $utilisateurRepository->findOneBy([
+            'id' => $technicienId,
+            'type_utilisateur' => 'technicien'
+        ]);
+        
+        if (!$intervention) {
+            $this->addFlash('danger', 'Intervention introuvable.');
             return $this->redirectToRoute('app_gerant_affectation');
         }
 
+        if (!$technicien) {
+            $this->addFlash('danger', 'Technicien non trouvé ou non autorisé.');
+            return $this->redirectToRoute('app_gerant_affectation');
+        }
+
+        // Si on arrive ici, intervention et technicien sont valides
+        $intervention->setTechnicien($technicien);
+        
+        // Gestion du client
+        if ($clientId) {
+            $client = $clientRepository->find($clientId);
+            if ($client) {
+                $intervention->setClient($client);
+            } else {
+                $this->addFlash('warning', 'Client non trouvé, l\'intervention a été mise à jour sans modification du client.');
+            }
+        }
+
+        // Gestion de la date de visite
+        if (!empty($dateVisiteStr)) {
+            try {
+                $dateVisite = \DateTime::createFromFormat('Y-m-d', $dateVisiteStr);
+                if ($dateVisite === false) {
+                    throw new \Exception('Format invalide');
+                }
+                $intervention->setDateVisite($dateVisite);
+            } catch (\Exception $e) {
+                $this->addFlash('warning', 'Format de date invalide. Utilisez le format AAAA-MM-JJ.');
+            }
+        }
+
+        // Gestion de l'heure de visite
+        if (!empty($heureVisiteStr)) {
+            try {
+                $heureVisite = \DateTime::createFromFormat('H:i', $heureVisiteStr);
+                if ($heureVisite === false) {
+                    throw new \Exception('Format invalide');
+                }
+                $intervention->setHeureVisite($heureVisite);
+            } catch (\Exception $e) {
+                $this->addFlash('warning', 'Format d\'heure invalide. Utilisez le format HH:MM.');
+            }
+        }
+
+        try {
+            $em->flush();
+            $this->addFlash('success', 'Intervention affectée au technicien avec succès.');
+        } catch (\Exception $e) {
+            $this->addFlash('danger', 'Une erreur est survenue lors de la mise à jour de l\'intervention.');
+        }
+
+    } else {
+        $this->addFlash('danger', 'Données d\'affectation incomplètes. Veuillez sélectionner une intervention et un technicien.');
+    }
+
+    return $this->redirectToRoute('app_gerant_affectation');
+}
+
         $interventions = $interventionRepository->findAll();
-        $techniciens   = $employeRepository->findAll();
+        $techniciens = $utilisateurRepository->findBy(['type_utilisateur' => 'technicien']);
         $clients       = $clientRepository->findAll();
 
         return $this->render('gestionnaire/affectation.html.twig', [
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Controller;
@@ -11,44 +10,22 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
-    }
-}
-
-=======
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-class LoginController extends AbstractController
+public function index(AuthenticationUtils $authenticationUtils, Request $request): Response
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+    // Si l'utilisateur est déjà connecté, on le déconnecte
+    if ($this->getUser() && $request->query->get('action') === 'logout') {
+        $this->get('security.token_storage')->setToken(null);
+        $request->getSession()->invalidate();
+        return $this->redirectToRoute('app_login');
     }
-}
 
->>>>>>> 92ccd186012cd8c8a72cdab441359542fab06fa8
+    // Gestion de la connexion normale
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('login.html.twig', [
+        'last_username' => $lastUsername,
+        'error' => $error,
+    ]);
+}
+}
